633c95a9d89dc275baf0e0ed13c19d92ee700ae5
Peng Wang <forwangpeng@gmail.com>
2013-04-27 15:26:00 +0800

    format code

--- suffix_array.py
+++ suffix_array.py
@@ -4,7 +4,6 @@
 # * Algorithm refered : 'Programming Pearls(Second Edition)' Chapter 15
 # * demo without effective build process. Build: O(n) + O(nlogn)
 
-
 def build_suffix_array(suffix_array, text_string):
     for i in range(len(text_string)):
         suffix_array.append(text_string[i:])
@@ -19,17 +18,14 @@ def comlen(str_i, str_i_next):
 
     return max_len
 
-
 if __name__ == '__main__':
     suffix_array = []
     text_string = "Ask not what your country can do for you, but what you can do for your country"
 
     build_suffix_array(suffix_array, text_string)
  
-    # Use Suffix Array to solve LRS   
-    # The Longest Repeated Substring
-    max_len = 0
-    max_idx = 0
+    # Use Suffix Array to solve LRS(The Longest Repeated Substring)
+    max_len, max_idx = 0, 0
     for i in range(len(text_string)-1):
         com_len = comlen(suffix_array[i], suffix_array[i+1])
         [max_len, max_idx] = [com_len, i] if com_len > max_len else [max_len, max_idx]
