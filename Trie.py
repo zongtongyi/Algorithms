10bcb34a2ee9d4c1e8952a17d3f464a9a319134b
Peng Wang <forwangpeng@gmail.com>
2013-05-09 19:48:56 +0800

    return partial_match while match fail
    return partial_match_prefix while search fail

--- Trie.py
+++ Trie.py
@@ -22,12 +22,14 @@ class Trie(object):
         node.word = word
 
     def search(self, word):
+        partial_match = []
         node = self.root
         for c in word:
             if c not in node.children:
-                return (False, None)
+                return (False, partial_match)
             node = node.children[c]
-        return (True, node.word) if node.word else (False, None)
+            partial_match.append(c)
+        return (True, node.word) if node.word else (False, partial_match)
 
     def max_word_length(self):
         return self.height(self)
@@ -43,18 +45,6 @@ class Trie(object):
     def height(self):
         return 0 if not self.root.children else self.__node_height([v for v in self.root.children.itervalues()])
 
-    def delete(self, word, node=None, i=0):
-        node = node if node else self.root
-        c = word[i]
-        if c in node.children:
-            child_node = node.children[c]
-            if len(word)==(i+1):
-                return node.children.pop(c) if len(child_node.children)==0 else False
-            elif self.delete(word, child_node, i+1):
-                return node.children.pop(c) if (len(child_node.children)==0 and not child_node.word) else False
-        return False
-
-
 if __name__ == '__main__':
     tree = Trie()
     tree.insert('abcd')
@@ -65,10 +55,5 @@ if __name__ == '__main__':
     a = tree.search('ab')
     a = tree.search('be')
     b = tree.search('best')
-    b_a = tree.delete('abcd')
-    c = tree.search('ab')
-    d = tree.search('abcd')
-    b_b = tree.delete('beg')
-    e = tree.search('be')
     f = tree.search('better')
     pass
