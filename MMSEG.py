5c2d0650a27cc58055465a2b5c4c1921537de165
Peng Wang <forwangpeng@gmail.com>
2013-05-30 23:41:10 +0800

    Add code to deal with Unknown-word

--- MMSEG.py
+++ MMSEG.py
@@ -50,23 +50,25 @@ class MMSEG(object):
 
     def word_seg_complex(self, corpus):
         i, corpus_len, self.corpus_seg_list = 0, len(corpus), []
-        chunk_list = []
         while i < corpus_len:
             s = corpus[i:min(i + self.max_word_len, corpus_len)]
+            tri_word_list, tf_list, chunk_list = [], [], []
             find, match = self.lexicon.search_tf(s) # prefix match
-            tri_word_list, tf_list = [], []
+            match = match if len(match)!=0 else [(s[0], '1')] # unknown word
             for word, tf in match:
                 tri_word_list.append(word)
                 tf_list.append(0 if len(word)!=0 else tf)
                 i2 = i + len(word)
                 s2 = corpus[i2:min(i2+self.max_word_len, corpus_len)]
                 find2, match2 = self.lexicon.search_tf(s2)
+                match2 = match2 if len(match2)!=0 else [(s2[0], '1')] # unknown word
                 for word2, tf2 in match2:
                     tri_word_list.append(word2)
                     tf_list.append(0 if len(word2)!=0 else tf2)
                     i3 = i2 + len(word2)
                     s3 = corpus[i3:min(i3+self.max_word_len, corpus_len)]
                     find3, match3 = self.lexicon.search_tf(s3)
+                    match3 = match3 if len(match3)!=0 else [(s3[0], '1')] # unknown word
                     for word3, tf3 in match3:
                         tri_word_list.append(word3)
                         tf_list.append(0 if len(word3)!=0 else tf3)
@@ -97,6 +99,8 @@ if __name__ == '__main__':
     mmseg.word_seg_simple(corpus.decode('utf-8'))
 
     corpus = "研究生命起源"
+    corpus = "据英国《每日邮报》１２月２６日报道，视频分享网站ＹｏｕＴｕｂｅ　上近日出现一段点击量约百万的搞笑影片。在短片里，一只６个月大的猫咪在小心翼翼地下楼梯，不料，一旁冷眼观看的同伴突然伸出“魔爪”，一把将其推了下去，所幸小猫没有受伤。　（来源：中国日报网）"
     mmseg.word_seg_complex(corpus.decode('utf-8'))
+    mmseg.print_seg()
 
     pass
