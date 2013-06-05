5f30ed40a2a8bf1cca281e524711f05016545b4b
Peng Wang <forwangpeng@gmail.com>
2013-06-05 23:33:04 +0800

    update 2.0, add new methods(for doc_similarity)
    add word_count property, update properties, add merge_tf() method, add
    fill_max_trie() method to extract top_n trie.

--- Trie/Trie_mmseg.py
+++ Trie/Trie_mmseg.py
@@ -14,35 +14,42 @@ class Node(object):
 
 class Trie(object):
     def __init__(self):
-        self.root = Node()
+        self.__root = Node()
+        self.__word_count = 0
 
     def insert(self, word):    # Accumlate TF while insert word
-        node = self.root
+        node = self.__root
         for c in word:
             if c not in node.children:
                 node.children[c] = Node()
             node = node.children[c]
+
+        self.__word_count = self.__word_count if node.word else (self.__word_count + 1)
         node.word, node.tf = word, (node.tf + 1)
     
     def insert_0tf(self, word):    # ignore TF(keep as 1) while insert word
-        node = self.root
+        node = self.__root
         for c in word:
             if c not in node.children:
                 node.children[c] = Node()
             node = node.children[c]
+
+        self.__word_count = self.__word_count if node.word else (self.__word_count + 1)
         node.word = word
 
     def insert_tf(self, word, tf):    # SET TF while insert word
-        node = self.root
+        node = self.__root
         for c in word:
             if c not in node.children:
                 node.children[c] = Node()
             node = node.children[c]
-        node.word, node.tf = word, tf
+
+        self.__word_count = self.__word_count if node.word else (self.__word_count + 1)
+        node.word, node.tf = word, (node.tf + tf)
     
     '''def search(self, word):
         partial_match = []
-        node = self.root
+        node = self.__root
         for c in word:
             if c not in node.children:
                 return (False, partial_match)
@@ -53,7 +60,7 @@ class Trie(object):
 
     def search_tf(self, word):
         partial_match = []
-        node = self.root
+        node = self.__root
         for c in word:
             if c not in node.children:
                 return (False, partial_match)
@@ -62,8 +69,9 @@ class Trie(object):
                 partial_match.append((node.word, node.tf))
         return (True, partial_match) if node.word else (False, partial_match)
 
+    @property
     def max_word_length(self):
-        return self.height()
+        return self.height
 
     def __node_height(self, node_list):
         if len(node_list) == 0:
@@ -73,13 +81,14 @@ class Trie(object):
             [children_node_list.extend( [v for v in node.children.itervalues()] ) for node in node_list]
             return 1 + self.__node_height(children_node_list)
 
+    @property
     def height(self):
-        return 0 if not self.root.children else self.__node_height([v for v in self.root.children.itervalues()])
+        return 0 if not self.__root.children else self.__node_height([v for v in self.__root.children.itervalues()])
 
     def traverse_BFS(self): # Breadth-First Search
         import Queue
         node_queue = Queue.Queue()
-        node_queue.put(self.root)
+        node_queue.put(self.__root)
         while not node_queue.empty():
             node = node_queue.get()
             if node.word:
@@ -90,10 +99,28 @@ class Trie(object):
     def merge(self, Node_trie): # this incoming trie's node must be Node type
         if not isinstance(Node_trie, Trie):
             raise MergeTypeError('Incoming trie is not type(Trie)')
+
         [self.insert(word) for word, _ in Node_trie.traverse_BFS()]
+
+    def merge_tf(self, Node_trie): # this incoming trie's node must be Node type
+        if not isinstance(Node_trie, Trie):
+            raise MergeTypeError('Incoming trie is not type(Trie)')
+
+        [self.insert_tf(word, tf) for word, tf in Node_trie.traverse_BFS()]
 
     def item_list(self, wordtf_list):
         [wordtf_list.append((tf, word)) for word, tf in self.traverse_BFS()]
+
+    def fill_max_trie(self, max_trie, top_n):
+        rank_list = []
+        [rank_list.append((tf, word)) for word, tf in self.traverse_BFS()]
+        rank_list.sort(key=lambda x: -x[0])
+
+        [max_trie.insert_tf(word, tf) for tf, word in rank_list[:top_n]]
+
+    @property
+    def word_count(self):
+        return self.__word_count
 
 
 if __name__ == '__main__':
